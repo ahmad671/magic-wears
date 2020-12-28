3c60aa2b3547593f425458e436ae44e33390bce0
ahmad671 <ahmad.arbab07@gmail.com>
2020-12-28 13:02:44 +0500

    commit

---
+++ assets/js/custom.js
@@ -0,0 +1,529 @@
+$(document).ready(function(){
+    $('#search').click(function(){
+        $('.menu-item').addClass('hide-item')
+        $('.search-form').addClass('active')
+        $('.close').addClass('active')
+        $('#search').hide()
+    })
+    $('.close').click(function(){
+        $('.menu-item').removeClass('hide-item')
+        $('.search-form').removeClass('active')
+        $('.close').removeClass('active')
+        $('#search').show()
+    })
+    
+})
+
+$('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    callbacks: {
+      elementParse: function(item) {
+        console.log(item.el[0].className);
+        if(item.el[0].className == 'video') {
+          item.type = 'iframe',
+          item.iframe = {
+             patterns: {
+               youtube: {
+                 index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+  
+                 id: 'v=', // String that splits URL in a two parts, second part should be %id%
+                  // Or null - full URL will be returned
+                  // Or a function that should return %id%, for example:
+                  // id: function(url) { return 'parsed id'; } 
+  
+                 src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe. 
+               },
+               vimeo: {
+                 index: 'vimeo.com/',
+                 id: '/',
+                 src: '//player.vimeo.com/video/%id%?autoplay=1'
+               },
+               gmaps: {
+                 index: '//maps.google.',
+                 src: '%id%&output=embed'
+               }
+             }
+          }
+        } else {
+           item.type = 'image',
+           item.tLoading = 'Loading image #%curr%...',
+           item.mainClass = 'mfp-img-mobile',
+           item.image = {
+             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+           }
+        }
+  
+      }
+    }
+  });
+
+$(document).ready(function(){
+	
+	// Lift card and show stats on Mouseover
+	$('#product-card').hover(function(){
+			$(this).addClass('animate');
+			$('div.carouselNext, div.carouselPrev').addClass('visible');			
+		 }, function(){
+			$(this).removeClass('animate');			
+			$('div.carouselNext, div.carouselPrev').removeClass('visible');
+	});	
+	
+	// Flip card to the back side
+	$('#view_details').click(function(){		
+		$('div.carouselNext, div.carouselPrev').removeClass('visible');
+		$('#product-card').addClass('flip-10');
+		setTimeout(function(){
+			$('#product-card').removeClass('flip-10').addClass('flip90').find('div.shadow').show().fadeTo( 80 , 1, function(){
+				$('#product-front, #product-front div.shadow').hide();			
+			});
+		}, 50);
+		
+		setTimeout(function(){
+			$('#product-card').removeClass('flip90').addClass('flip190');
+			$('#product-back').show().find('div.shadow').show().fadeTo( 90 , 0);
+			setTimeout(function(){				
+				$('#product-card').removeClass('flip190').addClass('flip180').find('div.shadow').hide();						
+				setTimeout(function(){
+					$('#product-card').css('transition', '100ms ease-out');			
+					$('#cx, #cy').addClass('s1');
+					setTimeout(function(){$('#cx, #cy').addClass('s2');}, 100);
+					setTimeout(function(){$('#cx, #cy').addClass('s3');}, 200);				
+					$('div.carouselNext, div.carouselPrev').addClass('visible');				
+				}, 100);
+			}, 100);			
+		}, 150);			
+	});			
+	
+	// Flip card back to the front side
+	$('#flip-back').click(function(){		
+		
+		$('#product-card').removeClass('flip180').addClass('flip190');
+		setTimeout(function(){
+			$('#product-card').removeClass('flip190').addClass('flip90');
+	
+			$('#product-back div.shadow').css('opacity', 0).fadeTo( 100 , 1, function(){
+				$('#product-back, #product-back div.shadow').hide();
+				$('#product-front, #product-front div.shadow').show();
+			});
+		}, 50);
+		
+		setTimeout(function(){
+			$('#product-card').removeClass('flip90').addClass('flip-10');
+			$('#product-front div.shadow').show().fadeTo( 100 , 0);
+			setTimeout(function(){						
+				$('#product-front div.shadow').hide();
+				$('#product-card').removeClass('flip-10').css('transition', '100ms ease-out');		
+				$('#cx, #cy').removeClass('s1 s2 s3');			
+			}, 100);			
+		}, 150);			
+		
+	});	
+
+    $('#portfolio').mixItUp();
+
+  
+	/* ----  Image Gallery Carousel   ---- */
+	
+	var carousel = $('#carousel ul');
+	var carouselSlideWidth = 335;
+	var carouselWidth = 0;	
+	var isAnimating = false;
+	
+	// building the width of the casousel
+	$('#carousel li').each(function(){
+		carouselWidth += carouselSlideWidth;
+	});
+	$(carousel).css('width', carouselWidth);
+	
+	// Load Next Image
+	$('div.carouselNext').on('click', function(){
+		var currentLeft = Math.abs(parseInt($(carousel).css("left")));
+		var newLeft = currentLeft + carouselSlideWidth;
+		if(newLeft == carouselWidth || isAnimating === true){return;}
+		$('#carousel ul').css({'left': "-" + newLeft + "px",
+							   "transition": "300ms ease-out"
+							 });
+		isAnimating = true;
+		setTimeout(function(){isAnimating = false;}, 300);			
+	});
+	
+	// Load Previous Image
+	$('div.carouselPrev').on('click', function(){
+		var currentLeft = Math.abs(parseInt($(carousel).css("left")));
+		var newLeft = currentLeft - carouselSlideWidth;
+		if(newLeft < 0  || isAnimating === true){return;}
+		$('#carousel ul').css({'left': "-" + newLeft + "px",
+							   "transition": "300ms ease-out"
+							 });
+	    isAnimating = true;
+		setTimeout(function(){isAnimating = false;}, 300);			
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+	// MDB Lightbox Init
+	$(function () {
+	  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+	});
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ! function($) {
+    "use strict";
+    $.fn.meanmenu = function(e) {
+        var n = {
+            meanMenuTarget: jQuery(this),
+            meanMenuContainer: ".mobile-nav",
+            meanMenuClose: "X",
+            meanMenuCloseSize: "18px",
+            meanMenuOpen: "<span /><span /><span />",
+            meanRevealPosition: "right",
+            meanRevealPositionDistance: "0",
+            meanRevealColour: "",
+            meanScreenWidth: "480",
+            meanNavPush: "",
+            meanShowChildren: !0,
+            meanExpandableChildren: !0,
+            meanExpand: "+",
+            meanContract: "-",
+            meanRemoveAttrs: !1,
+            onePage: !1,
+            meanDisplay: "block",
+            removeElements: ""
+        };
+        e = $.extend(n, e);
+        var a = window.innerWidth || document.documentElement.clientWidth;
+        return this.each(function() {
+            var n = e.meanMenuTarget,
+                t = e.meanMenuContainer,
+                r = e.meanMenuClose,
+                i = e.meanMenuCloseSize,
+                s = e.meanMenuOpen,
+                u = e.meanRevealPosition,
+                m = e.meanRevealPositionDistance,
+                l = e.meanRevealColour,
+                o = e.meanScreenWidth,
+                c = e.meanNavPush,
+                v = ".meanmenu-reveal",
+                h = e.meanShowChildren,
+                d = e.meanExpandableChildren,
+                y = e.meanExpand,
+                j = e.meanContract,
+                Q = e.meanRemoveAttrs,
+                f = e.onePage,
+                g = e.meanDisplay,
+                p = e.removeElements,
+                C = !1;
+            (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/Blackberry/i) || navigator.userAgent.match(/Windows Phone/i)) && (C = !0), (navigator.userAgent.match(/MSIE 8/i) || navigator.userAgent.match(/MSIE 7/i)) && jQuery("html").css("overflow-y", "scroll");
+            var w = "",
+                x = function() {
+                    if ("center" === u) {
+                        var e = window.innerWidth || document.documentElement.clientWidth,
+                            n = e / 2 - 22 + "px";
+                        w = "left:" + n + ";right:auto;", C ? jQuery(".meanmenu-reveal").animate({
+                            left: n
+                        }) : jQuery(".meanmenu-reveal").css("left", n)
+                    }
+                },
+                A = !1,
+                E = !1;
+            "right" === u && (w = "right:" + m + ";left:auto;"), "left" === u && (w = "left:" + m + ";right:auto;"), x();
+            var M = "",
+                P = function() {
+                    M.html(jQuery(M).is(".meanmenu-reveal.meanclose") ? r : s)
+                },
+                W = function() {
+                    jQuery(".mean-bar,.mean-push").remove(), jQuery(t).removeClass("mean-container"), jQuery(n).css("display", g), A = !1, E = !1, jQuery(p).removeClass("mean-remove")
+                },
+                b = function() {
+                    var e = "background:" + l + ";color:" + l + ";" + w;
+                    if (o >= a) {
+                        jQuery(p).addClass("mean-remove"), E = !0, jQuery(t).addClass("mean-container"), jQuery(".mean-container").prepend('<div class="mean-bar"><a href="#nav" class="meanmenu-reveal" style="' + e + '">Show Navigation</a><nav class="mean-nav"></nav></div>');
+                        var r = jQuery(n).html();
+                        jQuery(".mean-nav").html(r), Q && jQuery("nav.mean-nav ul, nav.mean-nav ul *").each(function() {
+                            jQuery(this).is(".mean-remove") ? jQuery(this).attr("class", "mean-remove") : jQuery(this).removeAttr("class"), jQuery(this).removeAttr("id")
+                        }), jQuery(n).before('<div class="mean-push" />'), jQuery(".mean-push").css("margin-top", c), jQuery(n).hide(), jQuery(".meanmenu-reveal").show(), jQuery(v).html(s), M = jQuery(v), jQuery(".mean-nav ul").hide(), h ? d ? (jQuery(".mean-nav ul ul").each(function() {
+                            jQuery(this).children().length && jQuery(this, "li:first").parent().append('<a class="mean-expand" href="#" style="font-size: ' + i + '">' + y + "</a>")
+                        }), jQuery(".mean-expand").on("click", function(e) {
+                            e.preventDefault(), jQuery(this).hasClass("mean-clicked") ? (jQuery(this).text(y), jQuery(this).prev("ul").slideUp(300, function() {})) : (jQuery(this).text(j), jQuery(this).prev("ul").slideDown(300, function() {})), jQuery(this).toggleClass("mean-clicked")
+                        })) : jQuery(".mean-nav ul ul").show() : jQuery(".mean-nav ul ul").hide(), jQuery(".mean-nav ul li").last().addClass("mean-last"), M.removeClass("meanclose"), jQuery(M).click(function(e) {
+                            e.preventDefault(), A === !1 ? (M.css("text-align", "center"), M.css("text-indent", "0"), M.css("font-size", i), jQuery(".mean-nav ul:first").slideDown(), A = !0) : (jQuery(".mean-nav ul:first").slideUp(), A = !1), M.toggleClass("meanclose"), P(), jQuery(p).addClass("mean-remove")
+                        }), f && jQuery(".mean-nav ul > li > a:first-child").on("click", function() {
+                            jQuery(".mean-nav ul:first").slideUp(), A = !1, jQuery(M).toggleClass("meanclose").html(s)
+                        })
+                    } else W()
+                };
+            C || jQuery(window).resize(function() {
+                a = window.innerWidth || document.documentElement.clientWidth, a > o, W(), o >= a ? (b(), x()) : W()
+            }), jQuery(window).resize(function() {
+                a = window.innerWidth || document.documentElement.clientWidth, C ? (x(), o >= a ? E === !1 && b() : W()) : (W(), o >= a && (b(), x()))
+            }), b()
+        })
+    }
+}(jQuery);
+    // Start Menu JS
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 50) {
+            $('.main-nav').addClass('menu-shrink');
+        } else {
+            $('.main-nav').removeClass('menu-shrink');
+        }
+    });
+
+    // Search Box JS
+    $('.search-toggle').addClass('closed');
+    $('.search-toggle .search-icon').on('click', function(e) {
+        if ($('.search-toggle').hasClass('closed')) {
+            $('.search-toggle').removeClass('closed').addClass('opened');
+            $('.search-toggle, .search-area').addClass('opened');
+            $('#search-terms').focus();
+        } else {
+            $('.search-toggle').removeClass('opened').addClass('closed');
+            $('.search-toggle, .search-area').removeClass('opened');
+        }
+    });
+
+
+    // Mean Menu JS
+    jQuery('.mean-menu').meanmenu({
+        meanScreenWidth: "991"
+    });
+
+    // Header Sticky
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 120) {
+            $('.navbar-area').addClass("is-sticky");
+        } else {
+            $('.navbar-area').removeClass("is-sticky");
+        }
+    });
+
+
+
+
+
+
+
+    function centerModal() {
+        $(this).css('display', 'block');
+        var $dialog = $(this).find(".modal-dialog");
+        var offset = ($(window).height() - $dialog.height()) / 2;
+        // Center modal vertically in window
+        $dialog.css("margin-top", offset);
+    }
+    
+    $('.modal').on('show.bs.modal', centerModal);
+    $(window).on("resize", function () {
+        $('.modal:visible').each(centerModal);
+    });
+
+
+
+
+    
+/* 
+   MixitUp
+   ========================================================================== */
+  $('#portfolio').mixItUp();
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
